00c12ed9fe24ab7678407a5287a43469e7508ea1
lee <lee@feeditback.com>
2024-07-11 14:33:01 +0100

    dupe from vue-tsc-conflicting-global-types

---
+++ packages/package-b/src/ComponentB.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ComponentA, BaseRow } from "package-a";
+import { ref } from "vue";
+
+type MyRow = BaseRow & { text: string };
+
+defineProps<{
+  propB: string;
+}>();
+
+const rows = ref<MyRow[]>([
+  { value: 1, text: "one" },
+  { value: 2, text: "two" },
+]);
+</script>
+
+<template>
+  <component-a :rows="rows" non-generic="something">
+    <template v-slot="{ row }"></template>
+  </component-a>
+</template>
+
+<style scoped></style>
